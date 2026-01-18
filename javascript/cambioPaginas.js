afd6b7c686bc56a73a2abbefcf998076fbcdace0
rrodriguez-2023342 <rrodriguez-2023342@kinal.edu.gt>
2026-01-17 20:38:30 -0600

    feat: Se hizo la pagina del index.html junto con su diseño, ademas se conecto con las demas paginas

---
+++ javascript/cambioPaginas.js
@@ -0,0 +1,14 @@
+// Metodo para ir a la vista de Mis Gustos
+function irMisGustos() {
+        window.location.href = "../html/misGustos.html";
+}
+
+// Metodo para ir a la vista de Biografia
+function irBiografia() {
+        window.location.href = "../html/biografia.html";
+}
+
+// Metodo para ir a la vista de Rediseño Kinal
+function irRediseñoKinal() {
+        window.location.href = "../html/rediseñoKinal.html";
+}
